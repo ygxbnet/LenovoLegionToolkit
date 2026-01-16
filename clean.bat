0450d784f790635cb72c43fc241bbdd7f14a8b9f
YGXB_net <ygxb-net@outlook.com>
2026-01-16 10:42:59 +0800

    更新构建脚本以使用src目录

--- clean.bat
+++ clean.bat
@@ -6,23 +6,23 @@ rmdir /s /q _ReSharper.Caches
 rmdir /s /q build
 rmdir /s /q build_installer
 
-rmdir /s /q LenovoLegionToolkit.CLI\bin
-rmdir /s /q LenovoLegionToolkit.CLI\obj
+rmdir /s /q src\LenovoLegionToolkit.CLI\bin
+rmdir /s /q src\LenovoLegionToolkit.CLI\obj
 
-rmdir /s /q LenovoLegionToolkit.Lib\bin
-rmdir /s /q LenovoLegionToolkit.Lib\obj
+rmdir /s /q src\LenovoLegionToolkit.Lib\bin
+rmdir /s /q src\LenovoLegionToolkit.Lib\obj
 
-rmdir /s /q LenovoLegionToolkit.Lib.Automation\bin
-rmdir /s /q LenovoLegionToolkit.Lib.Automation\obj
+rmdir /s /q src\LenovoLegionToolkit.Lib.Automation\bin
+rmdir /s /q src\LenovoLegionToolkit.Lib.Automation\obj
 
-rmdir /s /q LenovoLegionToolkit.Lib.CLI\bin
-rmdir /s /q LenovoLegionToolkit.Lib.CLI\obj
+rmdir /s /q src\LenovoLegionToolkit.Lib.CLI\bin
+rmdir /s /q src\LenovoLegionToolkit.Lib.CLI\obj
 
-rmdir /s /q LenovoLegionToolkit.Lib.Macro\bin
-rmdir /s /q LenovoLegionToolkit.Lib.Macro\obj
+rmdir /s /q src\LenovoLegionToolkit.Lib.Macro\bin
+rmdir /s /q src\LenovoLegionToolkit.Lib.Macro\obj
 
-rmdir /s /q LenovoLegionToolkit.WPF\bin
-rmdir /s /q LenovoLegionToolkit.WPF\obj
+rmdir /s /q src\LenovoLegionToolkit.WPF\bin
+rmdir /s /q src\LenovoLegionToolkit.WPF\obj
 
-rmdir /s /q LenovoLegionToolkit.SpectrumTester\bin
-rmdir /s /q LenovoLegionToolkit.SpectrumTester\obj
+rmdir /s /q src\LenovoLegionToolkit.SpectrumTester\bin
+rmdir /s /q src\LenovoLegionToolkit.SpectrumTester\obj
