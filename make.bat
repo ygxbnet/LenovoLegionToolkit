d51e5a981b414f62c428c26cf7ae487323be422b
YGXB_net <ygxb-net@outlook.com>
2026-01-16 10:56:03 +0800

    更新构建脚本以使用src目录

--- make.bat
+++ make.bat
@@ -9,8 +9,12 @@ IF "%1"=="" (
 
 SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
 
-dotnet publish src/LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
-dotnet publish src/LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
-dotnet publish src/LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+cd src
+
+dotnet publish LenovoLegionToolkit.WPF -c release -o ../build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o ../build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+dotnet publish LenovoLegionToolkit.CLI -c release -o ../build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+
+cd ..
 
 iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
