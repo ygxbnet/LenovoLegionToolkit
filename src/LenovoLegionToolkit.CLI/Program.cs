bc6ecc335fe76b3186d84f1051ded1e5ed924aab
YGXB_net <ygxb-net@outlook.com>
2026-01-18 14:25:01 +0800

    refactor(cli): 更新 System.CommandLine 库并重构命令行解析逻辑
    - 将 System.CommandLine 从版本 2.0.0-beta4.22272.1 升级到 2.0.2
    - 移除 CommandLineBuilder 和相关异常处理代码
    - 使用 Parse(args).InvokeAsync() 替代 InvokeAsync(args) 方法调用
    - 将命令和选项的添加方式从 AddCommand/Option 改为 Add/Options.Add/Arguments.Add
    - 将命令处理程序从 SetHandler 改为 SetAction 并适配新的参数传递方式
    - 将验证器错误消息设置从 ErrorMessage 改为 AddError 方法
    - 将别名添加方式从 AddAlias 改为 Aliases.Add 方法
    - 适配 ParseResult 的值获取方式从 FindResultFor 改为 GetResult/GetValue
    - 注释掉原有的异常处理方法 OnException

--- src/LenovoLegionToolkit.CLI/Program.cs
+++ src/LenovoLegionToolkit.CLI/Program.cs
@@ -1,67 +1,63 @@
 ﻿using System;
 using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Invocation;
-using System.CommandLine.IO;
-using System.CommandLine.Parsing;
+using System.Threading;
 using System.Threading.Tasks;
-using LenovoLegionToolkit.CLI.Lib;
 
 namespace LenovoLegionToolkit.CLI;
 
 public class Program
 {
-    public static Task<int> Main(string[] args) => BuildCommandLine().InvokeAsync(args);
+    public static Task<int> Main(string[] args) => BuildCommandLine().Parse(args).InvokeAsync();
 
-    private static Parser BuildCommandLine()
+    private static RootCommand BuildCommandLine()
     {
         var root = new RootCommand("Utility that controls Lenovo Legion Toolkit from command line.\n\n" +
                                    "Lenovo Legion Toolkit must be running in the background and CLI setting must be " +
                                    "turned on for this utility to work.");
 
-        var builder = new CommandLineBuilder(root)
-            .UseDefaults()
-            .UseExceptionHandler(OnException);
+        root.Add(BuildQuickActionsCommand());
+        root.Add(BuildFeatureCommand());
+        root.Add(BuildSpectrumCommand());
+        root.Add(BuildRGBCommand());
 
-        root.AddCommand(BuildQuickActionsCommand());
-        root.AddCommand(BuildFeatureCommand());
-        root.AddCommand(BuildSpectrumCommand());
-        root.AddCommand(BuildRGBCommand());
-
-        return builder.Build();
+        return root;
     }
 
     private static Command BuildQuickActionsCommand()
     {
-        var nameArgument = new Argument<string>("name", "Name of the Quick Action") { Arity = ArgumentArity.ZeroOrOne };
+        var nameArgument = new Argument<string>( "Name of the Quick Action") { Arity = ArgumentArity.ZeroOrOne };
 
         var listOption = new Option<bool>("--list", "List available Quick Actions") { Arity = ArgumentArity.ZeroOrOne };
-        listOption.AddAlias("-l");
+        listOption.Aliases.Add("-l");
 
         var cmd = new Command("quickAction", "Run Quick Action");
-        cmd.AddAlias("qa");
-        cmd.AddArgument(nameArgument);
-        cmd.AddOption(listOption);
-        cmd.SetHandler(async (name, list) =>
+        cmd.Aliases.Add("qa");
+        cmd.Arguments.Add(nameArgument);
+        cmd.Options.Add(listOption);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var name = parseResult.GetValue(nameArgument);
+            var list = parseResult.GetValue(listOption);
+
             if (list)
             {
                 var result = await IpcClient.ListQuickActionsAsync();
                 Console.WriteLine(result);
-                return;
+                return 0;
             }
 
             await IpcClient.RunQuickActionAsync(name);
-        }, nameArgument, listOption);
-        cmd.AddValidator(result =>
+            return 0;
+        });
+        cmd.Validators.Add(result =>
         {
-            if (result.FindResultFor(nameArgument) is not null)
+            if (result.GetResult(nameArgument) is not null)
                 return;
 
-            if (result.FindResultFor(listOption) is not null)
+            if (result.GetResult(listOption) is not null)
                 return;
 
-            result.ErrorMessage = $"{nameArgument.Name} or --{listOption.Name} should be specified";
+            result.AddError($"{nameArgument.Name} or --{listOption.Name} should be specified");
         });
 
         return cmd;
@@ -73,33 +69,36 @@ public class Program
         var setCmd = BuildSetFeatureCommand();
 
         var listOption = new Option<bool?>("--list", "List available features") { Arity = ArgumentArity.ZeroOrOne };
-        listOption.AddAlias("-l");
+        listOption.Aliases.Add("-l");
 
         var cmd = new Command("feature", "Control features");
-        cmd.AddAlias("f");
-        cmd.AddCommand(getCmd);
-        cmd.AddCommand(setCmd);
-        cmd.AddOption(listOption);
-        cmd.SetHandler(async list =>
+        cmd.Aliases.Add("f");
+        cmd.Add(getCmd);
+        cmd.Add(setCmd);
+        cmd.Options.Add(listOption);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var list = parseResult.GetValue(listOption);
+
             if (!list.HasValue || !list.Value)
-                return;
+                return 0;
 
             var value = await IpcClient.ListFeaturesAsync();
             Console.WriteLine(value);
-        }, listOption);
-        cmd.AddValidator(result =>
+            return 0;
+        });
+        cmd.Validators.Add(result =>
         {
-            if (result.FindResultFor(getCmd) is not null)
+            if (result.GetResult(getCmd) is not null)
                 return;
 
-            if (result.FindResultFor(setCmd) is not null)
+            if (result.GetResult(setCmd) is not null)
                 return;
 
-            if (result.FindResultFor(listOption) is not null)
+            if (result.GetResult(listOption) is not null)
                 return;
 
-            result.ErrorMessage = $"{getCmd.Name}, {setCmd.Name} or --{listOption.Name} should be specified";
+            result.AddError($"{getCmd.Name}, {setCmd.Name} or --{listOption.Name} should be specified");
         });
 
         return cmd;
@@ -107,53 +106,60 @@ public class Program
 
     private static Command BuildGetFeatureCommand()
     {
-        var nameArgument = new Argument<string>("name", "Name of the feature") { Arity = ArgumentArity.ExactlyOne };
+        var nameArgument = new Argument<string>( "Name of the feature") { Arity = ArgumentArity.ExactlyOne };
 
         var cmd = new Command("get", "Get value of a feature");
-        cmd.AddAlias("g");
-        cmd.AddArgument(nameArgument);
-        cmd.SetHandler(async name =>
+        cmd.Aliases.Add("g");
+        cmd.Arguments.Add(nameArgument);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var name = parseResult.GetValue(nameArgument);
             var result = await IpcClient.GetFeatureValueAsync(name);
             Console.WriteLine(result);
-        }, nameArgument);
+            return 0;
+        });
 
         return cmd;
     }
 
     private static Command BuildSetFeatureCommand()
     {
-        var nameArgument = new Argument<string>("name", "Name of the feature") { Arity = ArgumentArity.ExactlyOne };
-        var valueArgument = new Argument<string>("value", "Value of the feature") { Arity = ArgumentArity.ZeroOrOne };
+        var nameArgument = new Argument<string>( "Name of the feature") { Arity = ArgumentArity.ExactlyOne };
+        var valueArgument = new Argument<string>( "Value of the feature") { Arity = ArgumentArity.ZeroOrOne };
 
         var listOption = new Option<bool>("--list", "List available feature values") { Arity = ArgumentArity.ZeroOrOne };
-        listOption.AddAlias("-l");
+        listOption.Aliases.Add("-l");
 
         var cmd = new Command("set", "Set value of a feature");
-        cmd.AddAlias("s");
-        cmd.AddArgument(nameArgument);
-        cmd.AddArgument(valueArgument);
-        cmd.AddOption(listOption);
-        cmd.SetHandler(async (name, value, list) =>
+        cmd.Aliases.Add("s");
+        cmd.Arguments.Add(nameArgument);
+        cmd.Arguments.Add(valueArgument);
+        cmd.Options.Add(listOption);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var name = parseResult.GetValue(nameArgument);
+            var value = parseResult.GetValue(valueArgument);
+            var list = parseResult.GetValue(listOption);
+
             if (list)
             {
                 var result = await IpcClient.ListFeatureValuesAsync(name);
                 Console.WriteLine(result);
-                return;
+                return 0;
             }
 
             await IpcClient.SetFeatureValueAsync(name, value);
-        }, nameArgument, valueArgument, listOption);
-        cmd.AddValidator(result =>
+            return 0;
+        });
+        cmd.Validators.Add(result =>
         {
-            if (result.FindResultFor(nameArgument) is not null)
+            if (result.GetResult(nameArgument) is not null)
                 return;
 
-            if (result.FindResultFor(listOption) is not null)
+            if (result.GetResult(listOption) is not null)
                 return;
 
-            result.ErrorMessage = $"{nameArgument.Name} or --{listOption.Name} should be specified";
+            result.AddError($"{nameArgument.Name} or --{listOption.Name} should be specified");
         });
 
         return cmd;
@@ -165,9 +171,9 @@ public class Program
         var brightnessCommand = BuildSpectrumBrightnessCommand();
 
         var cmd = new Command("spectrum", "Control Spectrum backlight");
-        cmd.AddAlias("s");
-        cmd.AddCommand(profileCommand);
-        cmd.AddCommand(brightnessCommand);
+        cmd.Aliases.Add("s");
+        cmd.Add(profileCommand);
+        cmd.Add(brightnessCommand);
         return cmd;
     }
 
@@ -177,9 +183,9 @@ public class Program
         var setCmd = BuildSetSpectrumProfileCommand();
 
         var cmd = new Command("profile", "Control Spectrum backlight profile");
-        cmd.AddAlias("p");
-        cmd.AddCommand(getCmd);
-        cmd.AddCommand(setCmd);
+        cmd.Aliases.Add("p");
+        cmd.Add(getCmd);
+        cmd.Add(setCmd);
 
         return cmd;
     }
@@ -187,11 +193,12 @@ public class Program
     private static Command BuildGetSpectrumProfileCommand()
     {
         var cmd = new Command("get", "Get current Spectrum profile");
-        cmd.AddAlias("g");
-        cmd.SetHandler(async _ =>
+        cmd.Aliases.Add("g");
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
             var result = await IpcClient.GetSpectrumProfileAsync();
             Console.WriteLine(result);
+            return 0;
         });
 
         return cmd;
@@ -199,15 +206,17 @@ public class Program
 
     private static Command BuildSetSpectrumProfileCommand()
     {
-        var valueArgument = new Argument<int>("profile", "Profile to set") { Arity = ArgumentArity.ExactlyOne };
+        var valueArgument = new Argument<int>( "Profile to set") { Arity = ArgumentArity.ExactlyOne };
 
         var cmd = new Command("set", "Set current Spectrum profile");
-        cmd.AddAlias("s");
-        cmd.AddArgument(valueArgument);
-        cmd.SetHandler(async value =>
+        cmd.Aliases.Add("s");
+        cmd.Arguments.Add(valueArgument);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var value = parseResult.GetValue(valueArgument);
             await IpcClient.SetSpectrumProfileAsync($"{value}");
-        }, valueArgument);
+            return 0;
+        });
 
         return cmd;
     }
@@ -218,9 +227,9 @@ public class Program
         var setCmd = BuildSetSpectrumBrightnessCommand();
 
         var cmd = new Command("brightness", "Control Spectrum brightness");
-        cmd.AddAlias("b");
-        cmd.AddCommand(getCmd);
-        cmd.AddCommand(setCmd);
+        cmd.Aliases.Add("b");
+        cmd.Add(getCmd);
+        cmd.Add(setCmd);
 
         return cmd;
     }
@@ -228,11 +237,12 @@ public class Program
     private static Command BuildGetSpectrumBrightnessCommand()
     {
         var cmd = new Command("get", "Get current Spectrum brightness");
-        cmd.AddAlias("g");
-        cmd.SetHandler(async _ =>
+        cmd.Aliases.Add("g");
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
             var result = await IpcClient.GetSpectrumBrightnessAsync();
             Console.WriteLine(result);
+            return 0;
         });
 
         return cmd;
@@ -240,15 +250,17 @@ public class Program
 
     private static Command BuildSetSpectrumBrightnessCommand()
     {
-        var valueArgument = new Argument<int>("brightness", "Brightness to set") { Arity = ArgumentArity.ExactlyOne };
+        var valueArgument = new Argument<int>( "Brightness to set") { Arity = ArgumentArity.ExactlyOne };
 
         var cmd = new Command("set", "Set current Spectrum brightness");
-        cmd.AddAlias("s");
-        cmd.AddArgument(valueArgument);
-        cmd.SetHandler(async value =>
+        cmd.Aliases.Add("s");
+        cmd.Arguments.Add(valueArgument);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var value = parseResult.GetValue(valueArgument);
             await IpcClient.SetSpectrumBrightnessAsync($"{value}");
-        }, valueArgument);
+            return 0;
+        });
 
         return cmd;
     }
@@ -259,9 +271,9 @@ public class Program
         var setCmd = BuildSetRGBCommand();
 
         var cmd = new Command("rgb", "Control RGB backlight preset");
-        cmd.AddAlias("r");
-        cmd.AddCommand(getCmd);
-        cmd.AddCommand(setCmd);
+        cmd.Aliases.Add("r");
+        cmd.Add(getCmd);
+        cmd.Add(setCmd);
 
         return cmd;
     }
@@ -269,11 +281,12 @@ public class Program
     private static Command BuildGetRGBCommand()
     {
         var cmd = new Command("get", "Get current RGB preset");
-        cmd.AddAlias("g");
-        cmd.SetHandler(async _ =>
+        cmd.Aliases.Add("g");
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
             var result = await IpcClient.GetRGBPresetAsync();
             Console.WriteLine(result);
+            return 0;
         });
 
         return cmd;
@@ -281,46 +294,48 @@ public class Program
 
     private static Command BuildSetRGBCommand()
     {
-        var valueArgument = new Argument<int>("preset", "Preset to set") { Arity = ArgumentArity.ExactlyOne };
+        var valueArgument = new Argument<int>( "Preset to set") { Arity = ArgumentArity.ExactlyOne };
 
         var cmd = new Command("set", "Set current RGB preset");
-        cmd.AddAlias("s");
-        cmd.AddArgument(valueArgument);
-        cmd.SetHandler(async value =>
+        cmd.Aliases.Add("s");
+        cmd.Arguments.Add(valueArgument);
+        cmd.SetAction(async (ParseResult parseResult, CancellationToken token) =>
         {
+            var value = parseResult.GetValue(valueArgument);
             await IpcClient.SetRGBPresetAsync($"{value}");
-        }, valueArgument);
+            return 0;
+        });
 
         return cmd;
     }
 
-    private static void OnException(Exception ex, InvocationContext context)
-    {
-        var message = ex switch
-        {
-            IpcConnectException => "Failed to connect. " +
-                                   "Make sure that Lenovo Legion Toolkit is running " +
-                                   "in background and CLI is enabled in Settings.",
-            IpcException => ex.Message,
-            _ => ex.ToString()
-        };
-        var exitCode = ex switch
-        {
-            IpcConnectException => -1,
-            IpcException => -2,
-            _ => -99
-        };
-
-        if (!Console.IsOutputRedirected)
-        {
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-        }
-
-        context.Console.Error.WriteLine(message);
-        context.ExitCode = exitCode;
-
-        if (!Console.IsOutputRedirected)
-            Console.ResetColor();
-    }
+    // private static void OnException(Exception ex, InvocationContext context)
+    // {
+    //     var message = ex switch
+    //     {
+    //         IpcConnectException => "Failed to connect. " +
+    //                                "Make sure that Lenovo Legion Toolkit is running " +
+    //                                "in background and CLI is enabled in Settings.",
+    //         IpcException => ex.Message,
+    //         _ => ex.ToString()
+    //     };
+    //     var exitCode = ex switch
+    //     {
+    //         IpcConnectException => -1,
+    //         IpcException => -2,
+    //         _ => -99
+    //     };
+    //
+    //     if (!Console.IsOutputRedirected)
+    //     {
+    //         Console.ResetColor();
+    //         Console.ForegroundColor = ConsoleColor.Red;
+    //     }
+    //
+    //     context.Console.Error.WriteLine(message);
+    //     context.ExitCode = exitCode;
+    //
+    //     if (!Console.IsOutputRedirected)
+    //         Console.ResetColor();
+    // }
 }
